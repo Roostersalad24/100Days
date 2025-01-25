ad20ddb57299d05c7418e35d53942b7e5a6c2a61
Roostersalad24 <andrewjohnsonmga@gmail.com>
2025-01-25 15:20:02 -0600

    add error keywords

--- throwing errors.playground/Contents.swift
+++ throwing errors.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//create enum to create cases where errors can be thrown
+
+enum PasswordError: Error {
+    case obvious
+}
+
+// type "throws" to enable the function to throw an error
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw  PasswordError.obvious
+    }
+    return true
+}
+
+//Swift requires new keywords for errors
+//three new keywords:
+// "do" starts a section of code that might cause problems
+// "try" is used before every function that might throw an error
+// "catch" lets you handle errors gracefully.
+
+
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
