a50a80164d6dde57408057ff820789f3f9c2452b
Roostersalad24 <andrewjohnsonmga@gmail.com>
2025-01-26 01:10:11 -0600

    closure returns

--- closure returns.playground/Contents.swift
+++ closure returns.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//closures return values by placing directly before "in"
+
+let driving = { (place: String) in
+        print("I'm going to \(place) in my car")
+}
+
+//can add return by placing it between parenthesis and "in"
+let drivingWithReturn = { (place: String) -> String in
+        return "I'm going to \(place) in my car"
+}
+
+//now use the closure to print the return value
+let message = drivingWithReturn("London")
+print(message)
+
+//a closure doesn't need a parameter to return, but it needs empty parenthesis
+let payment = { () -> Bool in
+    print("Paying an anonymous person")
+    return true
+}
