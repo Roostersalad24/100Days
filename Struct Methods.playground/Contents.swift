c017ba947a73ee2e53c49304157c7e0fba00b7d5
Roostersalad24 <andrewjohnsonmga@gmail.com>
2025-02-06 17:57:19 -0600

    Struct Methods

--- Struct Methods.playground/Contents.swift
+++ Struct Methods.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Methods are functions that are inside a struct
+//the function below collectTaxes() is a method that belongs to City
+
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+//now that method can be called
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+let dallas = City(population: 1_300_000)
+dallas.collectTaxes()
