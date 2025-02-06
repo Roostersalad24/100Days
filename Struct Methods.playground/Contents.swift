a603854edb6262d70ffc2e77902308878b9aa9e7
Roostersalad24 <andrewjohnsonmga@gmail.com>
2025-02-06 17:57:19 -0600

    Struct Methods

--- Struct Methods.playground/Contents.swift
+++ Struct Methods.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Methods are functions that are inside a struct
+//the function below collectTaxes() is a method that belongs to City
+
+struct City {
+    var population: Int
+    
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+//now that method can be called
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+let dallas = City(population: 1_300_000)
+dallas.collectTaxes()
+
+//Methods are useful for avoiding function name pollution that reserve names in code. 100 functions would mean 100 names are reserved. Methods are restricted to only be called along with their parent struct
